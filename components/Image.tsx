50eb70f7660746f4959db7fb924c68ee081a3e3f
shubhra <shubhras01@gmail.com>
2024-01-16 11:35:14 +0530

    adding files

---
+++ components/Image.tsx
@@ -0,0 +1,8 @@
+import NextImage, { ImageProps } from 'next/image'
+
+const Image = ({ src, ...rest }: ImageProps) => (
+    <NextImage src={`/writeups${src}`} {...rest} />
+  )
+  
+
+export default Image
