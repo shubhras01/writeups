6065964a1890865fc382db28af38dd73e8496735
shubhra <shubhras01@gmail.com>
2024-01-16 13:55:07 +0530

    adding nsa task 4

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,3 +3,9 @@ import NextImage, { ImageProps } from 'next/image'
 const Image = ({ src, ...rest }: ImageProps) => <NextImage src={`/writeups${src}`} {...rest} />
 
 export default Image
+
+// import NextImage, { ImageProps } from 'next/image'
+
+// const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+
+// export default Image
